a3c951be2ee4678778f5d60f10719ac0c609c69d
Ar Rakin <rakinar2@onesoftnet.eu.org>
2024-12-21 17:55:35 +0600

    feat!: switch to Next.js
    Signed-off-by: Ar Rakin <rakinar2@onesoftnet.eu.org>

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    basePath: "/",
+    output: "export",
+};
+
+export default nextConfig;
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import './style.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
-
-createApp(App).use(router).mount('#app');
